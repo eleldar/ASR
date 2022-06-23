e3cf99574f00d14d3d4c64b30462cd022187b3f6
eldar <eleldar@mail.ru>
2022-06-23 19:41:52 +0300

    time score of recognitions steps

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask_restx import Resource, Api
 from werkzeug.datastructures import FileStorage
 from random import randint, choice
 from api.recognitions import recognize
+from time import time
+from datetime import timedelta
 
 # Local path settings
 drive, path_and_file = os.path.splitdrive(Path(__file__).absolute())
@@ -32,16 +34,35 @@ upload_parser.add_argument('file', location='files',
 @api.expect(upload_parser)
 class Upload(Resource):
     def post(self):
+        times = {
+            'start': None,
+            'get_prefix': None,
+            'uploaded_file': None,
+            'recognize': None,
+        }
         args = upload_parser.parse_args()
         uploaded_file = args['file']  # This is FileStorage instance
+        times['start'] = time()
         file_prefix = get_file_prefix()
+        times['get_prefix'] = time()
         dir_local_path = os.path.join(curdir, 'api', 'tempfiles', 'src') 
         file_local_path = os.path.join(
             dir_local_path,  
             f'{file_prefix}_{uploaded_file.filename}'
         )
         uploaded_file.save(file_local_path)
-        return {'message': recognize(file_local_path)}, 200
+        times['uploaded_file'] = time()  
+        text = recognize(file_local_path)
+        times['recognize'] = time()  
+
+        response = {
+            'message': recognize(file_local_path),
+            'get_prefix': str(timedelta(seconds = times['get_prefix'] - times['start'])),
+            'uploaded_file': str(timedelta(seconds = times['uploaded_file'] - times['get_prefix'])),
+            'recognize': str(timedelta(seconds = times['recognize'] - times['uploaded_file'])),
+            'full': str(timedelta(seconds = times['recognize'] - times['start'])),
+        } 
+        return response, 200
 
 
 if __name__ == '__main__':
