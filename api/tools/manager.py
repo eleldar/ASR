9cc948419039cbfc683d7f4baf96f3dc5df716e6
eldar <eleldar@mail.ru>
2022-06-27 16:06:58 +0300

    thread-based upload file, detect file type, update time score report

--- api/tools/manager.py
+++ api/tools/manager.py
@@ -10,7 +10,6 @@ from api.tools.handler import Handler
 drive, path_and_file = os.path.splitdrive(Path(__file__).absolute())
 path, file = os.path.split(path_and_file)
 curdir = os.path.join(drive, path)
-print(f'**********************{curdir}**************************')
 
 alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 get_file_prefix = lambda: ''.join(choice(alphabet) for _ in range(32))
@@ -39,7 +38,6 @@ class Manager(object):
             dir_local_path,  
             f'{task_id}_{file.filename}'
         )
-        print(file_path)
         file.save(file_path)
         handler = Handler()
         threading.Thread(target=handler.start, args=(file_path,), daemon=True).start()
@@ -56,131 +54,10 @@ class Manager(object):
     @check_tasks
     def get_status(self, task_id):
         handler_idx = self.task_ids.index(task_id)
-        error, current_frame, num_frames = self.handlers[handler_idx].get_status()  # obtained status
-        if current_frame >= num_frames or error != '':
-            os.remove(f'{task_id}.mp4')
-        return error, current_frame, num_frames
+        return self.handlers[handler_idx].get_status() 
 
     def get_tasks(self):
         return self.task_ids
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-
-
-import os
-import sys
-from pathlib import Path
-from flask import Flask
-from flask_restx import Resource, Api, fields
-from flask_cors import CORS, cross_origin
-from werkzeug.datastructures import FileStorage
-from random import randint, choice
-from api.recognitions import recognize
-from time import time
-from datetime import timedelta
-# api/tools/src/manager
-from api.tools.src.manager import Manager
-
-
-# Local path settings
-drive, path_and_file = os.path.splitdrive(Path(__file__).absolute())
-path, file = os.path.split(path_and_file)
-curdir = os.path.join(drive, path)
-
-app = Flask(__name__)
-api = Api(
-    app, 
-    version='1.0',
-    title='ASR',
-    description='ASR',
-    doc="/api",
-)
-
-CORS(app)
-namespace = api.namespace('api', description='ASR_cors?')
-
-start_response = api.model('StartResponse', {
-    'task_id': fields.String
-})
-
-
-alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-get_file_prefix = lambda: ''.join(choice(alphabet) for _ in range(32))
-
-upload_parser = api.parser()
-upload_parser.add_argument('file', location='files',
-                            type=FileStorage, required=True
-                          )
-manager = Manager()
-
-@api.route('/one_video')
-@api.expect(upload_parser)
-class Upload(Resource):
-    def post(self):
-        times = {
-            'start': None,
-            'get_prefix': None,
-            'uploaded_file': None,
-            'recognize': None,
-        }
-        args = upload_parser.parse_args()
-        uploaded_file = args['file']  # This is FileStorage instance
-        times['start'] = time()
-        file_prefix = get_file_prefix()
-        times['get_prefix'] = time()
-        dir_local_path = os.path.join(curdir, 'api', 'tempfiles', 'src') 
-        file_local_path = os.path.join(
-            dir_local_path,  
-            f'{file_prefix}_{uploaded_file.filename}'
-        )
-        uploaded_file.save(file_local_path)
-        times['uploaded_file'] = time()  
-        text = recognize(file_local_path)
-        times['recognize'] = time()  
-
-        response = {
-            'message': recognize(file_local_path),
-            'get_prefix': str(timedelta(seconds = times['get_prefix'] - times['start'])),
-            'uploaded_file': str(timedelta(seconds = times['uploaded_file'] - times['get_prefix'])),
-            'recognize': str(timedelta(seconds = times['recognize'] - times['uploaded_file'])),
-            'full': str(timedelta(seconds = times['recognize'] - times['start'])),
-        } 
-        return response, 200
-
-
-
-@namespace.route('/start')
-class DetectApi(Resource):
-    @namespace.doc('VideoHandleStart')
-    @namespace.expect(upload_parser)
-    @namespace.marshal_with(start_response, code=201)
-    def post(self):
-        args = upload_parser.parse_args()
-        file = args['file']
-
-        task_id = manager.start(file)
-        return {'task_id': task_id}, 201
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-"""
 
 
 
